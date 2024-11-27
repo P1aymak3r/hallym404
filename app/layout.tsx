29002921bd6284ef5a8202cf3f892f4e212e4a21
P1aymak3r <74403502+P1aymak3r@users.noreply.github.com>
2024-11-27 18:03:59 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
-'use client'
+'use client';
 
-import './globals.css'
-import { AuthProvider } from './context/AuthContext'
-import { ProfileProvider } from './context/ProfileContext'
-import React from 'react'
-import { UserProvider } from './context/UserContext'
+import './globals.css';
+import { AuthProvider } from './context/AuthContext';
+import { ProfileProvider } from './context/ProfileContext';
+import React from 'react';
+import { UserProvider } from './context/UserContext';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={inter.className}>
+      <Head>
+        <title>WearShare</title>
+        <meta name="description" content="Your one-stop shop for trendy clothing" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <AuthProvider>
           <ProfileProvider>
@@ -23,5 +32,5 @@ export default function RootLayout({
         </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
